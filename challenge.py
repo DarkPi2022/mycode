59e037929b7a840e12afac32ea7841afd9aff87b
Jerry <student@bchd.39616076-09d8-4953-ac6c-7e6d642d8292>
2022-05-26 14:57:35 +0000

    Learning to parse log files

--- challenge.py
+++ challenge.py
@@ -4,9 +4,9 @@ challenge= ["science", "turbo", ["goggles", "eyes"], "nothing"]
 
 a= challenge[2][1]
 b= challenge[2][0]
-c= challenge[3]
+c= challenge[3]  # can also use -1
 
-#print(f"My {a}! The {b} do {c}!")
+print(f"My {a}! The {b} do {c}!")
 
 trial= ["science", "turbo", {"eyes": "goggles", "goggles": "eyes"}, "nothing"]
 
@@ -14,7 +14,7 @@ a= trial[2]["goggles"]
 b= trial[2]["eyes"]
 c= trial[-1]
 
-#print(f"My {a}! The {b} do {c}!")
+print(f"My {a}! The {b} do {c}!")
 
 
 nightmare= [{"slappy": "a", "text": "b", "kumquat": "goggles", "user":{"awesome": "c", "name": {"first": "eyes", "last": "toes"}},"banana": 15, "d": "nothing"}]
@@ -23,7 +23,6 @@ a= nightmare[0]["user"]["name"]["first"]
 b= nightmare[0]["kumquat"]
 c= nightmare[0]["d"]
 
-#print(f"My {a}! The {b} do {c}!")
+print(f"My {a}! The {b} do {c}!")
 
-print(nightmare[0]["user"]['name']["first"])
 
